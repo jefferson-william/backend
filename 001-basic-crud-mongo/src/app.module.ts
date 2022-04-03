30ca2491ab70c723858bbc4195cffbf91f231b4b
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2022-04-03 00:36:24 -0300

    feat(users): crud

--- 001-basic-crud-mongo/src/app.module.ts
+++ 001-basic-crud-mongo/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_HOST), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
